8e4cec3c709c3e61b9f7d1c65a056c2d6537e5c9
“jacquelinegomez06” <jacquelinegomez06@gmail.com>
2023-03-05 12:48:44 -0500

    data extraction complete

--- extract.py
+++ extract.py
@@ -1,32 +1,36 @@
-from sqlalchemy import create_engine, func
+from config import connection
+from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from config import connection
 import pandas as pd
+import functools as ft
 
-#prepairing connection to database
-engine = create_engine(connection).connect()
 
-#allows querying database(pulling data)
+engine = create_engine(connection)
+
 session = Session(engine)
 
-# this object will automatically map our db entity into a Python class
 Base = automap_base()
-# get db into automapper
-Base.prepare(engine, reflect=True)
+Base.prepare(engine, reflect = True)
 
-#mapped classes are created with default matching of the table names
 jobs = Base.classes.jobs
-salaries = Base.classes.salaries
 skills = Base.classes.skills
+salaries = Base.classes.salaries 
 
-#a query with filtering and selection
-jobs_rows = session.query(jobs)
-salaries_rows=session.query(salaries)
-skills_rows=session.query(skills)
+jobs_result = session.query(jobs)
+skills_result = session.query(skills)
+salaries_result = session.query(salaries)
 
-jobs_df = pd.read_sql(jobs_rows.statement, engine)
-skills_df = pd.read_sql(skills_rows.statement, engine)
-salaries_df = pd.read_sql(salaries_rows.statement, engine)
+jobs_df = pd.read_sql(sql = jobs_result.statement, con = engine.connect())
+skills_df = pd.read_sql(sql = skills_result.statement, con = engine.connect())
+salaries_df = pd.read_sql(sql = salaries_result.statement, con = engine.connect())
 
+engine.dispose()
 
+#merge 3 tables on primary key id
+dfs = [jobs_df,skills_df,salaries_df]
+df_final = ft.reduce(lambda left, right: pd.merge(left, right, on='id'), dfs)
+df_merged = ft.reduce(lambda  left,right: pd.merge(left,right,on=['id'], how='outer'), dfs)
+
+# Write this joined dataframe to the data / folder
+df_merged.to_csv("data/joined_data.csv", index = False)

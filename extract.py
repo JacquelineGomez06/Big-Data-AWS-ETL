d5abd55c8793b1adbeb943f5551162c53b6b1598
“jacquelinegomez06” <jacquelinegomez06@gmail.com>
2023-03-05 11:50:03 -0500

    file structure

--- extract.py
+++ extract.py
@@ -2,19 +2,31 @@ from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from config import connection
+import pandas as pd
 
-engine = create_engine(connection)
+#prepairing connection to database
+engine = create_engine(connection).connect()
+
+#allows querying database(pulling data)
+session = Session(engine)
 
 # this object will automatically map our db entity into a Python class
 Base = automap_base()
-
 # get db into automapper
 Base.prepare(engine, reflect=True)
 
-# save classes as variables, prepare classes
+#mapped classes are created with default matching of the table names
 jobs = Base.classes.jobs
 salaries = Base.classes.salaries
 skills = Base.classes.skills
 
-# query our database (pull data and save into objects)
-session = Session(engine)
+#a query with filtering and selection
+jobs_rows = session.query(jobs)
+salaries_rows=session.query(salaries)
+skills_rows=session.query(skills)
+
+jobs_df = pd.read_sql(jobs_rows.statement, engine)
+skills_df = pd.read_sql(skills_rows.statement, engine)
+salaries_df = pd.read_sql(salaries_rows.statement, engine)
+
+
